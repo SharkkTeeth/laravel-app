076b8f1d18f3fb975e5fa40f728bb17c2deb4c31
SharkkTeeth <maraaraya47@gmail.com>
2024-03-13 09:28:14 -0300

    configuracion, uso de migracion

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom</title>
+</head>
+<body>
+    <h1><?php echo $msj ?></h1>
+    <a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente a cupiditate, quibusdam aperiam magni iure ducimus, illo reprehenderit inventore, eos alias veritatis perferendis maxime necessitatibus. Adipisci aperiam qui blanditiis! Ipsam.</a>
+    <h2><?= $msj ?></h2>
+    <a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente a cupiditate, quibusdam aperiam magni iure ducimus, illo reprehenderit inventore, eos alias veritatis perferendis maxime necessitatibus. Adipisci aperiam qui blanditiis! Ipsam.</a>
+    <h3>{{ $msj }}</h3>
+    <a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente a cupiditate, quibusdam aperiam magni iure ducimus, illo reprehenderit inventore, eos alias veritatis perferendis maxime necessitatibus. Adipisci aperiam qui blanditiis! Ipsam.</a>
+    
+</body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
-    
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
